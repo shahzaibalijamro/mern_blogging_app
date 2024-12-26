4e83c6ec85099acb27d6ff20a08809f156d593da
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-12-26 22:30:30 +0500

    commands added

--- back_end/index.js
+++ back_end/index.js
@@ -5,10 +5,33 @@ import { blogRouter } from "./src/routes/blogs.routes.js"
 import { userRouter } from "./src/routes/users.routes.js"
 import { connectDB } from "./src/db/index.js"
 import { authRouter } from "./src/routes/auth.routes.js"
+import mongoose from "mongoose"
 
 app.use("/api/v1", blogRouter)
 app.use("/api/v1", userRouter)
 app.use("/api/v1", authRouter)
+
+app.get("/api/v1/health", async (req, res) => {
+    try {
+        await mongoose.connection.db.admin().ping();
+        res.status(200).json({
+            status: "success",
+            message: "Server is healthy",
+            data: {
+                uptime: process.uptime(),
+                timestamp: new Date(),
+            },
+        });
+    } catch (error) {
+        console.error("Health check failed:", error);
+
+        res.status(500).json({
+            status: "error",
+            message: "Server is down or encountering an issue",
+        });
+    }
+});
+
 
 connectDB()
     .then(() => {
